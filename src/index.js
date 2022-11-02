89ffefc5c72587bbb908ef02d06a61f882b8480f
Henrique <henrique.lopesdesouza@gmail.com>
2022-11-02 19:31:25 -0300

    season update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // only moment that is ok to use a direct assignment
-    this.state = { lat: null };
+    this.state = { lat: null, erroMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ erroMessage: err.message })
 
-        // not ok to use => this.state.lat = position.coords.latitude
-      },
-      (err) => {
-        this.setState({ erroMessage: err.message });
-      }
+      // not ok to use => this.state.lat = position.coords.latitude
     );
   }
 
@@ -24,7 +23,7 @@ class App extends React.Component {
       return <div>Error: {this.state.erroMessage}</div>;
     }
     if (!this.state.erroMessage && this.state.lat) {
-      return <div>Lat: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
